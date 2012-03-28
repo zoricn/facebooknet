20a5688af8ef3b6c089703ae42d7a52d35b36ea3
Nebojsa Zoric <zoricn@gmail.com>
2012-03-28 20:42:27 +0200

    canvas working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,28 +6,14 @@ class PagesController < ApplicationController
   #before_filter :verify_access_token, :only => "index"
 
   def index
-     @oauth = Koala::Facebook::OAuth.new(ENV['AST_FACEBOOK_APP_ID'], ENV['AST_FACEBOOK_APP_SECRET'])
+    @oauth = Koala::Facebook::OAuth.new(ENV['AST_FACEBOOK_APP_ID'], ENV['AST_FACEBOOK_APP_SECRET'])
     access_token = @oauth.parse_signed_request(params[:signed_request])["oauth_token"]
-    puts access_token
     @graph = Koala::Facebook::API.new(access_token)
-    @profile = @graph.get_object("me")
-   @current_token = access_token
+    @current_person = @graph.get_object("me")
+    @friends = get_friends(@graph)  
   end
 
   def dashboard
-#    url = session['oauth'].url_for_access_token(params[:code])
-		#result = session['oauth'].fetch_token_string(params[:code])
-		# => "access_token=#{access_token}&expires=#{seconds_from_now}"
-#body = response
-#JSON.parse(body)['access_token']
-
-    #access_token = 	session['oauth'].parse_token_string(params[:code])
-    #request_authorization
-#session['oauth'].get_access_token(params[:code])
-debugger
-    @graph = Koala::Facebook::API.new(params[:code])
-    @fb = @graph.get_object("me")
-    #@friends = get_friends(@graph)
   end
 
   private
