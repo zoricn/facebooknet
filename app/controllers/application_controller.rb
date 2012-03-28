7e8b9aa8ddfffb68f537f7096142b664a972e02c
Nebojsa Zoric <zoricn@gmail.com>
2012-03-28 20:50:47 +0200

    clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #layout :dynamic_layout
 
   def render404
     render :file => File.join(Rails.root, 'public', '404.html'), :status => 404, :layout => false
@@ -23,62 +22,5 @@ class ApplicationController < ActionController::Base
   def current_token
     session['access_token']
   end
-
-  # Control which layout is used.
-  def dynamic_layout
-    if current_user.nil?
-      'application'
-    else
-      'dashboard'
-    end
-  end
-=begin
-  def request_authorization
-      # FB doesn't like port numbers in the return URL in canvas apps so only use for local dev
-=begin
-      @oauth = Koala::Facebook::OAuth.new(ENV['AST_FACEBOOK_APP_ID'], ENV['AST_FACEBOOK_APP_SECRET'],"http://astrology.local:3000/dashboard/")
-      info1 = @oauth.get_user_info_from_cookies(cookies)
-debugger
-      redirect_to @oauth.url_for_oauth_code(:permissions => 'user_birthday,friends_birthday')#session['oauth'].url_for_oauth_code(:permissions => 'user_birthday,friends_birthday')
-
-      full_host = request.host
-      full_host += ':' + request.port.to_s if request.host == 'localhost'
-      return_url = "#{request.protocol}#{full_host}#{request.fullpath}"
-      logger.info 'FB Auth Return URL: ' + return_url
-      session['oauth'] = Koala::Facebook::OAuth.new(ENV['AST_FACEBOOK_APP_ID'], ENV['AST_FACEBOOK_APP_SECRET'], "http://astrology.local:3000/dashboard/")
-      redirect_to session['oauth'].url_for_oauth_code(:permissions => 'user_birthday,friends_birthday')
-  end
-=end
-  private
-
-    def request_authorization
-      # FB doesn't like port numbers in the return URL in canvas apps so only use for local dev
-      full_host = request.host
-      full_host += ':' + request.port.to_s if request.host == 'localhost'
-      return_url = "#{request.protocol}#{full_host}#{request.fullpath}"
-      logger.info 'FB Auth Return URL: ' + return_url
-      session['oauth'] = Koala::Facebook::OAuth.new(ENV['AST_FACEBOOK_APP_ID'], ENV['AST_FACEBOOK_APP_SECRET'], return_url)
-      redirect_to session['oauth'].url_for_oauth_code(:permissions => 'user_birthday,friends_birthday')
-    end
-
-    def verify_access_token
-      unless session['access_token']
-        if session['oauth'] && params[:code]
-          # Use the code returned by Facebook to get an access token
-          session['access_token'] = session['oauth'].get_access_token(params[:code])
-        else
-          # Coming in cold so ask for auth
-          request_authorization && return
-        end
-      end
-      @graph = Koala::Facebook::API.new(session['access_token'])
-
-      # Need to get an actual object to test the access token
-      @profile = @graph.get_object("me")
-
-    rescue Koala::Facebook::APIError
-      session['access_token'] = nil
-      request_authorization
-    end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,17 @@
 require 'koala'
 require 'json'
 class PagesController < ApplicationController
-  #before_filter :authenticate_user!, :except => "index"
-  #before_filter :auth_koala!, :only => "dashboard"
-  #before_filter :verify_access_token, :only => "index"
+  before_filter :get_access_token, :only => "index"
 
   def index
-    @oauth = Koala::Facebook::OAuth.new(ENV['AST_FACEBOOK_APP_ID'], ENV['AST_FACEBOOK_APP_SECRET'])
-    access_token = @oauth.parse_signed_request(params[:signed_request])["oauth_token"]
-    @graph = Koala::Facebook::API.new(access_token)
-    @current_person = @graph.get_object("me")
-    @friends = get_friends(@graph)  
+    begin
+ 	    @graph = Koala::Facebook::API.new(@access_token)
+      @current_person = @graph.get_object("me")
+      @friends = get_friends(@graph)  
+    rescue Koala::Facebook::APIError
+    	logs "Koala API error"
+    end
+
   end
 
   def dashboard
@@ -19,7 +20,7 @@ class PagesController < ApplicationController
   private
 
   def auth_koala!
-    @graph = Koala::Facebook::GraphAPI.new(current_user.token)
+    @graph = Koala::Facebook::API.new(@access_token)
   end
 
   def get_friends(graph)
@@ -27,11 +28,9 @@ class PagesController < ApplicationController
     friends.sort! { |a,b| a["name"].downcase <=> b["name"].downcase }.first 20
   end
 
-
-  
-  def koala_oauth
-      session['oauth'] = Koala::Facebook::OAuth.new(ENV['AST_FACEBOOK_APP_ID'], ENV['AST_FACEBOOK_APP_SECRET'])
-  	
+  def get_access_token
+    oauth = Koala::Facebook::OAuth.new(ENV['AST_FACEBOOK_APP_ID'], ENV['AST_FACEBOOK_APP_SECRET']) 
+    @access_token = oauth.parse_signed_request(params[:signed_request])["oauth_token"] 	
   end
 
 end
