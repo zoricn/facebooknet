c3ac156bbbe7dcede1607dc4c694c302e29b7136
Nebojsa Zoric <zoricn@gmail.com>
2012-03-30 10:25:29 +0200

    typo

--- lib/koala_service.rb
+++ lib/koala_service.rb
@@ -1,11 +1,11 @@
 module KoalaService
   def self.graph_from(token)
-    Koala::Facebook::API.new(@access_token)
+    Koala::Facebook::API.new(token)
   end
 
   def self.get_twenty_friends(graph)
     friends = graph.get_connections("me", "friends", :fields => "birthday,name")
-    return friends.sort! { |a,b| a["name"].downcase <=> b["name"].downcase }.first 20 || []
+    return friends.sort! { |a,b| a["name"].downcase <=> b["name"].downcase }.first 20
   end
 
 end
